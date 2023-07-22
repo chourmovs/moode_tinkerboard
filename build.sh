5c80d4d5ca2ec3bdda17605f50357404f20d166f
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-07-22 22:02:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -223,7 +223,7 @@ fpm -s dir -t deb -n $PKGNAME -v $PKGVERSION \
 --category sound \
 -S moode \
 --iteration $DEBVER$DEBLOC \
--a all \
+# -a all \
 --deb-priority optional \
 --url https://moodeaudio.org \
 -m moodeaudio.org \
