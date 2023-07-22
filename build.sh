a18197f0431ad066040582a38254be56616f0ca2
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-07-23 00:27:56 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -222,13 +222,7 @@ cat $BASE_DIR/postinstall.sh | sed -e "s/^PKG_VERSION=.*/PKG_VERSION=\"$PKGVERSI
 
 # Don't include packages as dependency, if those package depends on the used kernel (like drivers).
 # Install those separate.
-fpm -s dir -t deb -n $PKGNAME -v $PKGVERSION \
---license GPLv3 \
---category sound \
--S moode \
---iteration $DEBVER$DEBLOC \
---after-install $POSTINSTALL2 \
---config-files $CAMILLA1 \
+fpm --config-files $CAMILLA1 \
 --config-files $CAMILLA2 \
 --depends alsa-cdsp \
 --depends alsacap \
@@ -319,6 +313,12 @@ fpm -s dir -t deb -n $PKGNAME -v $PKGVERSION \
 --depends xorg \
 --depends zip \
 --config-files $CONFIG2 \
+-s dir -t deb -n $PKGNAME -v $PKGVERSION \
+--license GPLv3 \
+--category sound \
+-S moode \
+--iteration $DEBVER$DEBLOC \
+--after-install $POSTINSTALL2 \
 root/boot/.=/boot \
 root/var/.=/var \
 root/home/.=/home \
