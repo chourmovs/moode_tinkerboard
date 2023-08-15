c17a1a0ac8f89e98c25c0a82e3165cc3c5a2f989
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-08-15 21:52:22 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -36,6 +36,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update -y
 sudo apt install -y docker-ce
+sleep 3
 sudo usermod -aG docker $USER
 
 sleep 3
