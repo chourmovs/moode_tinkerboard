898841cfc8c3c23bfd91054183574ff93c251407
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-08-15 22:53:00 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -31,7 +31,7 @@ echo ""
 
 
 sudo apt update -y
-sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common screen
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update -y
