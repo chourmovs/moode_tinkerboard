07331ffdb7f5a3535629ba04008c496de500fac0
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-08-27 14:19:09 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -134,7 +134,7 @@ sleep 2
 
 docker exec -ti debian-moode /bin/bash -c "sudo sed -i 's/#Port 22/Port 2222/g' /etc/ssh/sshd_config;"
 docker exec -ti debian-moode /bin/bash -c "sudo service sshd restart"
-
+docker exec -ti debian-moode /bin/bash -c "apt --fix-broken install -y"
 
 docker exec -ti debian-moode /bin/bash -c "curl -1sLf  'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' | sudo -E distro=raspbian codename=bullseye arch=armv7hf bash -"
 docker exec -ti debian-moode /bin/bash -c "apt-get update -y |apt-get install moode-player -y --fix-missing"
